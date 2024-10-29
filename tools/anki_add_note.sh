36c72b9df5dfd0ef683bdc0eff408afe1abf2a27
Einar Persson <me@einarpersson.com>
2024-10-29 20:28:23 +0100

    add anki_add_note

---
+++ tools/anki_add_note.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+# @describe Add a new note to Anki with front and back text
+# @option --front! The front text of the card
+# @option --back! The back text of the card
+
+main() {
+    if apy add-single "$argc_front" "$argc_back"; then
+        echo "Added new Anki note with front: '$argc_front' and back: '$argc_back'"
+    fi
+}
+
+eval "$(argc --argc-eval "$0" "$@")"
