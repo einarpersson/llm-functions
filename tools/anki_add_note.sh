228addc2b5718e699ae3d34d02f826251f83d336
Einar Persson <me@einarpersson.com>
2025-01-21 07:07:50 +0100

    update anki add note

--- tools/anki_add_note.sh
+++ tools/anki_add_note.sh
@@ -4,11 +4,14 @@ set -e
 # @describe Add a new note to Anki with front and back text
 # @option --front! The front text of the card
 # @option --back! The back text of the card
+# @option --tag Tags to add to the card (separate multiple tags with spaces, e.g. "tag1 tag2 tag3")
 
 main() {
-    if apy add-single "$argc_front" "$argc_back"; then
-        "Added new Anki note with front: '$argc_front' and back: '$argc_back'" >> "$LLM_OUTPUT"
-    fi
+    # Add the note to Anki
+    apy add-single ${argc_tag:+-t "$argc_tag"} "$argc_front" "$argc_back"
+    
+    # If we get here, the command succeeded (due to set -e)
+    "Added new Anki note with front: '$argc_front' and back: '$argc_back'${argc_tag:+ and tag: '$argc_tag'}" >> "$LLM_OUTPUT"
 }
 
 eval "$(argc --argc-eval "$0" "$@")"
