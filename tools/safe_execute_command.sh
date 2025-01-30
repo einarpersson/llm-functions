477429066b83581af3bbe0f3a8934d82e9046f28
Einar Persson <me@einarpersson.com>
2025-01-30 15:27:30 +0100

    make safe exeuction a bit smarter

--- tools/safe_execute_command.sh
+++ tools/safe_execute_command.sh
@@ -9,11 +9,36 @@ set -e
 ROOT_DIR="${LLM_ROOT_DIR:-$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)}"
 
 main() {
-    if ! "$ROOT_DIR/utils/guard_operation.sh"; then
-        echo "IMPORANT: OPERATION DISALLOWED BY USER, HALT IMMEDIATELY AND WAIT FOR FURTHER INSTRUCTIONS FROM USER." >> "$LLM_OUTPUT"
-    elif ! eval "$argc_command" >> "$LLM_OUTPUT"; then
-        echo "Failed to execute the command, please HALT and wait for further instructions." >> "$LLM_OUTPUT"
-    fi
+
+		base_prompt="Your job is to make a assesment on whether a shell command is safe to execute or not. It is considered safe if it has NO SIDE-EFFECTS. If it is safe answer 'yes' otherwise answer 'no'. Only exact matches are accepted. Only answer with one word.
+
+		If you receive any special considerations, take them into account when making your assessment."
+
+		if [ -n "${AICHAT_SAFE_EXECUTE:-}" ]; then
+			base_prompt="$base_prompt
+
+			Special considerations:
+			$AICHAT_SAFE_EXECUTE"
+		fi
+
+		is_safe_ai_assessment=$(aichat -m 'openai:gpt-4-turbo' -S --prompt "$base_prompt" $argc_command)
+
+		# If the aichat_safe is not 'yes' then we should trigger the guard operation.
+		if [ "$is_safe_ai_assessment" != "yes" ]; then
+			# Trigger the guard operation
+			if ! "$ROOT_DIR/utils/guard_operation.sh"; then
+				echo "IMPORANT: OPERATION DISALLOWED BY USER, HALT IMMEDIATELY AND WAIT FOR FURTHER INSTRUCTIONS FROM USER." >> "$LLM_OUTPUT"
+			  exit 0
+			fi
+		else
+			# Show a message to the user
+			echo "The command is assessed as safe to execute."
+		fi
+
+		# If the aichat_safe is 'yes' then
+		if ! eval "$argc_command" >> "$LLM_OUTPUT"; then
+			echo "Failed to execute the command, please HALT and wait for further instructions." >> "$LLM_OUTPUT"
+		fi
 }
 
 eval "$(argc --argc-eval "$0" "$@")"
